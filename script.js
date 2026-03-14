a91828f1fe14ffceee6e67c35a6193f187f5e2e0
HELGA-M-asset <olg.mantur@gmail.com>
2026-03-14 23:21:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // ѕодсветка активного пункта меню при скролле
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.main-nav a[href^="#"]');
+    
+    window.addEventListener('scroll', () => {
+        let current = '';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (scrollY >= sectionTop - 200) {
+                current = section.getAttribute('id');
+            }
+        });
+        
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === `#${current}`) {
+                link.classList.add('active');
+            }
+        });
+    });
+});
